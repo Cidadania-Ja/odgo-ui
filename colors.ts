715aa7ae37ea42665c82270f7f69dc7470088507
Guilherme Albert <guilhermealbert14@gmail.com>
2023-09-21 16:40:20 -0300

    test

---
+++ colors.ts
@@ -0,0 +1,43 @@
+const colorsPrimary100 = "#ae1e22";
+const colorsPrimary90 = "#cc2328";
+const colorsPrimary80 = "#da252b";
+const colorsPrimary70 = "#dd3b40";
+const colorsPrimary60 = "#e15155";
+const colorsPrimary50 = "#e5676a";
+const colorsPrimary40 = "#e97c80";
+const colorsPrimary30 = "#ec9295";
+const colorsPrimary20 = "#f4bebf";
+const colorsPrimary10 = "#ffe0e1";
+const colorsSecondary100 = "#2c5e30";
+const colorsSecondary90 = "#346f39";
+const colorsSecondary80 = "#3b7d40";
+const colorsSecondary70 = "#438e49";
+const colorsSecondary60 = "#4ba052";
+const colorsSecondary50 = "#67b66e";
+const colorsSecondary40 = "#75bd7b";
+const colorsSecondary30 = "#91ca95";
+const colorsSecondary20 = "#a9d6ac";
+const colorsSecondary10 = "#cfe8d1";
+
+export const Colors = {
+    colorsPrimary100,
+    colorsPrimary90,
+    colorsPrimary80,
+    colorsPrimary70,
+    colorsPrimary60,
+    colorsPrimary50,
+    colorsPrimary40,
+    colorsPrimary30,
+    colorsPrimary20,
+    colorsPrimary10,
+    colorsSecondary100,
+    colorsSecondary90,
+    colorsSecondary80,
+    colorsSecondary70,
+    colorsSecondary60,
+    colorsSecondary50,
+    colorsSecondary40,
+    colorsSecondary30,
+    colorsSecondary20,
+    colorsSecondary10,
+}
